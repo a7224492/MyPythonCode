749887acdee3bde1154a19c415c8b79cbd00f8b0
a7224492 <a7224492.314158258@qq.com>
2017-06-22 16:18:16 +0800

    添加查询当前路径最大目录的脚本

---
+++ listDirSizeCurrPath.py
@@ -0,0 +1,107 @@
+import os
+
+class DirSizeDescBuilder:
+    def __init__(self):
+        self.name = ""
+        self.size = 0
+        self.type = ""
+
+    def setName(self, name):
+        self.name = name
+
+        return self
+
+    def setSize(self, size):
+        self.size = size
+
+        return self
+
+    def setType(self, type):
+        self.type = type
+
+        return self
+
+    def build(self):
+        return DirSizeDesc(self)
+
+class DirSizeDesc:
+    def __init__(self, builder):
+        self.name = builder.name
+        self.size = builder.size
+        self.type = builder.type
+
+    def __str__(self):
+        postfix = ""
+        size = 0.0
+        kunit = 1024.0
+        munit = kunit*1024
+        gunit = munit*1024
+        if self.size > gunit:
+            postfix = "G"
+            size = self.size/gunit
+        elif self.size > munit:
+            postfix = "M"
+            size = self.size/munit
+        elif self.size > kunit:
+            postfix = "K"
+            size = self.size/kunit
+        else:
+            postfix = "b"
+            size = self.size
+        return "".join(["[ ", str(self.name), ", ", str(round(size, 2)), " ", postfix, ", ", str(self.type), " ]"])
+
+class DirSize:
+    def __init__(self):
+        self.descList = []
+
+    def __calculateDirSize(self, path):
+        size = 0;
+        if os.path.isfile(path):
+            size = os.path.getsize(path)
+        else:
+            files = os.listdir(path)
+            for file in files:
+                filepath = os.path.join(path, file)
+                if os.path.isfile(filepath):
+                    size += os.path.getsize(filepath)
+                elif os.path.isdir(filepath):
+                    size += self.__calculateDirSize(filepath)
+
+        return size
+
+    def calculateSize(self, dirPath):
+        if os.path.isfile(dirPath):
+            return os.path.getsize(dirPath)
+
+        files = os.listdir(dirPath)
+        for file in files:
+            path = os.path.join(dirPath, file)
+            desc = DirSizeDescBuilder()
+            desc.setName(file)
+            desc.setSize(self.__calculateDirSize(path))
+            if os.path.isfile(path):
+                desc.setType("file")
+            elif os.path.isdir(path):
+                desc.setType("dir")
+
+            self.descList.append(desc.build())
+rootPath = os.getcwd()
+dirSize = DirSize()
+dirSize.calculateSize(rootPath)
+
+def dirSizeCmp(x, y):
+    if x.size > y.size:
+        return -1
+    elif x.size == y.size:
+        return 0
+    elif x.size < y.size:
+        return 1
+
+dirSize.descList.sort(cmp=dirSizeCmp)
+
+for desc in dirSize.descList:
+    print desc
+#desc = DirSizeDesc()
+#desc.name = "test"
+#desc.size = 10
+#desc.type = "file"
